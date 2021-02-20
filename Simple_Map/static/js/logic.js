8a551b3b104a32d421edfaee94598106dab200ea
mcginav <mcginav@yahoo.com>
2021-02-20 13:06:53 -0800

    adding Mapping_Single_Points code

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -9,6 +9,9 @@ let map = L.map('mapid', {
     zoom: 4
 });
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
